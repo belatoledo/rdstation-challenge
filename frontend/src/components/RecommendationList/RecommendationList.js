4990a7a9de5eaf4a55266ecc498aad99861e5c88
belatoledo <it.amancio@gmail.com>
2025-07-30 12:25:07 -0300

    update recommendation list

--- frontend/src/components/RecommendationList/RecommendationList.js
+++ frontend/src/components/RecommendationList/RecommendationList.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 function RecommendationList({ recommendations }) {
   return (
-    <div>
+    <div className="max-w-md h-full mx-auto p-6 bg-purple-50 border-2 border-purple-200 rounded-lg">
       <h2 className="text-lg font-bold mb-4">Lista de Recomendações:</h2>
 
       {recommendations.length === 0 && <p>Nenhuma recomendação encontrada.</p>}
