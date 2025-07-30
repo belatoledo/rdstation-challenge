a948878a653cc4d0dfdd373eb5c360dafea46858
belatoledo <it.amancio@gmail.com>
2025-07-30 12:48:31 -0300

    add recommendations length validation in list

--- frontend/src/components/RecommendationList/RecommendationList.js
+++ frontend/src/components/RecommendationList/RecommendationList.js
@@ -6,7 +6,12 @@ function RecommendationList({ recommendations }) {
     <div className="max-w-md h-full mx-auto p-6 bg-purple-50 border-2 border-purple-200 rounded-lg">
       <h2 className="text-lg font-bold mb-4">Lista de Recomendações:</h2>
 
-      {recommendations.length === 0 && <p>Nenhuma recomendação encontrada.</p>}
+      {recommendations.length === 0 && (
+        <p className="mt-8 px-8 text-purple-950 text-center font-light">
+          Selecione suas preferências e clique em
+          <strong>"Obter recomendação"</strong> para ver os resultados! ✨
+        </p>
+      )}
 
       <ul className="space-y-4 mt-6">
         {recommendations.map((recommendation) => (
