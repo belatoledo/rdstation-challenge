3f0dbeb05634acb43b2ec0c09c2bd3cf9d0a8a5c
belatoledo <it.amancio@gmail.com>
2025-07-30 14:03:23 -0300

    chore: adjust score logics

--- frontend/src/hooks/useRecommendations.js
+++ frontend/src/hooks/useRecommendations.js
@@ -1,13 +1,13 @@
 // useRecommendations.js
 
-import { useState } from 'react';
-import recommendationService from '../services/recommendation.service';
+import { useState } from "react";
+import getRecommendationsService from "../services/recommendation.service";
 
 function useRecommendations(products) {
   const [recommendations, setRecommendations] = useState([]);
 
   const getRecommendations = (formData) => {
-    return recommendationService.getRecommendations(formData, products);
+    return getRecommendationsService(formData, products);
   };
 
   return { recommendations, getRecommendations, setRecommendations };
