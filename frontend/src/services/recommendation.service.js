0c1b21be493ae76262a9390d938c695615b7f3d1
belatoledo <it.amancio@gmail.com>
2025-07-29 21:45:04 -0300

    feat: filter by recommendation type

--- frontend/src/services/recommendation.service.js
+++ frontend/src/services/recommendation.service.js
@@ -1,7 +1,11 @@
 // getRecommendations.js
 
 const getRecommendations = (formData = {}, products = []) => {
-  const { selectedPreferences = [], selectedFeatures = [] } = formData;
+  const {
+    selectedPreferences = [],
+    selectedFeatures = [],
+    selectedRecommendationType = "MultipleProducts",
+  } = formData;
 
   if (
     (selectedPreferences.length === 0 && selectedFeatures.length === 0) ||
@@ -30,12 +34,22 @@ const getRecommendations = (formData = {}, products = []) => {
     (product) => product.score > 0
   );
 
+  if (filteredProducts.length === 0) {
+    return [];
+  }
+
   const sortedProducts = filteredProducts.sort((a, b) => {
     if (b.score !== a.score) {
       return b.score - a.score;
     }
     return b.id - a.id;
   });
+
+  if (selectedRecommendationType === "SingleProduct") {
+    const topScoreProduct = sortedProducts[0];
+    const { score, ...cleanProduct } = topScoreProduct;
+    return [cleanProduct];
+  }
 
   const cleanProducts = sortedProducts.map(({ score, ...product }) => product);
 
